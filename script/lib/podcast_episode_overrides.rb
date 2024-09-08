c0b3bef2443b43e42b6af4bcabb229b3437c133d
Parker Moore <parkrmoore@gmail.com>
2024-09-08 09:17:45 -0700

    Update some more links with overrides

--- script/lib/podcast_episode_overrides.rb
+++ script/lib/podcast_episode_overrides.rb
@@ -28,6 +28,45 @@ PODCAST_EPISODE_OVERRIDES = {
   347403146283723 => {
     "episodeURL" => "https://radiolab.org/podcast/brown-box",
   },
+  3848733243628029 => {
+    "episodeURL" => "https://www.civics101podcast.org/civics-101-episodes/marbury-madison-trump",
+  },
+  3848733174590288 => {
+    "episodeURL" => "https://www.civics101podcast.org/civics-101-episodes/14thamendment",
+  },
+  3848733242793716 => {
+    "episodeURL" => "https://www.civics101podcast.org/civics-101-episodes/socialism-communism-fascism",
+  },
+  3848733544968689 => {
+    "episodeURL" => "https://www.civics101podcast.org/civics-101-episodes/fairnessdoctrine",
+  },
+  3848733510852601 => {
+    "episodeURL" => "https://www.civics101podcast.org/civics-101-episodes/federalreserve",
+  },
+  3848733515092572 => {
+    "episodeURL" => "https://www.civics101podcast.org/civics-101-episodes/votingpt1",
+  },
+  3848733182392811 => {
+    "episodeURL" => "https://www.civics101podcast.org/civics-101-episodes/usps",
+  },
+  3848733089889847 => {
+    "episodeURL" => "https://www.civics101podcast.org/civics-101-episodes/apgov",
+  },
+  3848733286935417 => {
+    "episodeURL" => "https://www.civics101podcast.org/civics-101-episodes/independents",
+  },
+  3848733872372962 => {
+    "episodeURL" => "https://www.civics101podcast.org/civics-101-episodes/dealth-penalty",
+  },
+  3848733908312898 => {
+    "episodeURL" => "https://www.civics101podcast.org/civics-101-episodes/foia",
+  },
+  4597337823061699 => {
+    "episodeURL" => "https://www.searchengine.show/listen/search-engine-1/is-there-a-sane-way-to-follow-this-election",
+  },
+  4597337412177133 => {
+    "episodeURL" => "https://www.searchengine.show/listen/search-engine-1/how-do-i-find-new-music-now-that-i-m-old-and-irrelevant",
+  },
   472563439795800 => {
     "episodeURL" => "https://rickhanson.com/being-well-podcast-how-to-change-your-life-self-efficacy-learned-helplessness-and-creating-a-growth-mindset/",
   },
--- script/lib/podcast_overrides.rb
+++ script/lib/podcast_overrides.rb
@@ -1,8 +1,12 @@
 PODCAST_OVERRIDES = {
   2630127 => {
     "podcastURL" => "https://www.nytimes.com/the-daily",
+    "episodeURL" => "https://www.nytimes.com/the-daily",
   },
   3584412 => {
     "podcastURL" => "https://www.civics101podcast.org/",
+  },
+  4281604 => {
+    "podcastURL" => "https://www.searchengine.show/",
   },
 }
